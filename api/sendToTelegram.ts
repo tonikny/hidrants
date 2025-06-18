ab19df9f1c7a2f813aff0f3445f6d927984a5d61
tonikny <dalecanya@gmail.com>
2025-06-18 20:12:13 +0200

    provaa

--- api/sendToTelegram.ts
+++ api/sendToTelegram.ts
@@ -1,4 +1,7 @@
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.status(200).json({ ok: true });
 }
 

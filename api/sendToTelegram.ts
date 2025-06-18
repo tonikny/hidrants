52e17cea27101e270711f700c38435ff6006dc48
tonikny <dalecanya@gmail.com>
2025-06-18 20:20:24 +0200

    pro

--- api/sendToTelegram.ts
+++ api/sendToTelegram.ts
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');

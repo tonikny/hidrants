f7570e0e7e7bfb6d6716987b1f0b3592477262e7
tonikny <dalecanya@gmail.com>
2025-06-18 20:37:19 +0200

    canvi api a js

--- api/sendToTelegram.js
+++ api/sendToTelegram.js
@@ -1,6 +1,4 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-module.exports = function handler(req: VercelRequest, res: VercelResponse) {
+module.exports = function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');

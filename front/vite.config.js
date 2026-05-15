d7da7d3105a378d378555ec1d60fcdb2e586dc40
tonikny <dalecanya@gmail.com>
2026-05-15 21:32:56 +0200

    upgrade vite 5 -> 8

--- front/vite.config.js
+++ front/vite.config.js
@@ -12,9 +12,29 @@ export default defineConfig({
     port: 3003,
     proxy: {
       '/api': {
-        target: 'http://localhost:3033',
+        target: 'http://127.0.0.1:3033',
         changeOrigin: false,
         secure: false,
+      },
+      '/tiles/osm': {
+        target: 'https://tile.openstreetmap.org',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/tiles\/osm/, ''),
+      },
+      '/tiles/opentopo': {
+        target: 'https://tile.opentopomap.org',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/tiles\/opentopo/, ''),
+      },
+      '/tiles/ign-raster': {
+        target: 'https://tms-mapa-raster.ign.es/1.0.0/mapa-raster',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/tiles\/ign-raster/, ''),
+      },
+      '/tiles/ign-orto': {
+        target: 'https://tms-pnoa-ma.idee.es/1.0.0/pnoa-ma',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/tiles\/ign-orto/, ''),
       },
     },
   },

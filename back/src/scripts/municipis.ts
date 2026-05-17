4864da1afff82e7cbe778280a78e818b55ddb1c9
tonikny <dalecanya@gmail.com>
2026-05-17 13:35:02 +0200

    update

--- back/src/scripts/municipis.ts
+++ back/src/scripts/municipis.ts
@@ -1,1 +1,7 @@
-export const MUNICIPIS_NOMS = ['els Hostalets de Pierola', 'Piera', 'Masquefa'];
+export const MUNICIPIS_NOMS = [
+  'els Hostalets de Pierola',
+  'Piera',
+  'Masquefa',
+  "Vallbona d'Anoia",
+  'la Torre de Claramunt',
+];

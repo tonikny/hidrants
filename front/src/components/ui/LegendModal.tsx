7b276b5c8e39f4d69dfa3c97a03b502a80c0f3c3
tonikny <dalecanya@gmail.com>
2026-05-24 12:14:50 +0200

    canvi icones

--- front/src/components/ui/LegendModal.tsx
+++ front/src/components/ui/LegendModal.tsx
@@ -1,21 +1,28 @@
 import { useState } from 'react';
+import {
+  hidrant_nop_nrev,
+  hidrant_nop_rev,
+  hidrant_op_nrev,
+  hidrant_op_rev,
+} from '../../utils/icons';
 
 export function LegendModal({ style }: { style?: React.CSSProperties }) {
   const [open, setOpen] = useState(false);
   const currentYear = new Date().getFullYear();
   const styles = { display: 'flex', alignItems: 'center', gap: '15px' };
+  const iconsDir = '/images/icons';
 
   return (
     <>
       {/* Botó d'informació */}
-      <button 
-        onClick={() => setOpen(true)} 
+      <button
+        onClick={() => setOpen(true)}
         style={{
           ...style,
           fontWeight: 'bold',
           fontFamily: 'serif',
           fontStyle: 'italic',
-        }} 
+        }}
         title="Llegenda"
       >
         i
@@ -57,20 +64,19 @@ export function LegendModal({ style }: { style?: React.CSSProperties }) {
           >
             <h4>Hidrants</h4>
             <div style={styles}>
-              <img src="/images/icons/marker-icon-blue.png" /> Operatiu (revisat
+              <img src={iconsDir + hidrant_op_rev} /> Operatiu (revisat
               {currentYear})
             </div>
             <div style={styles}>
-              <img src="/images/icons/marker-icon-grey.png" /> Operatiu (no
+              <img src={iconsDir + hidrant_op_nrev} /> Operatiu (no revisat)
+            </div>
+            <div style={styles}>
+              <img src={iconsDir + hidrant_nop_rev} /> Fora de servei (
+              {currentYear})
+            </div>
+            <div style={styles}>
+              <img src={iconsDir + hidrant_nop_nrev} /> Fora de servei (no
               revisat)
-            </div>
-            <div style={styles}>
-              <img src="/images/icons/marker-icon-red.png" /> Fora de servei (
-              {currentYear})
-            </div>
-            <div style={styles}>
-              <img src="/images/icons/marker-icon-orange.png" /> Fora de servei
-              (no revisat)
             </div>
           </div>
         </button>
--- front/src/utils/icons.tsx
+++ front/src/utils/icons.tsx
@@ -1,5 +1,12 @@
 import L from 'leaflet';
 
+export const hidrant_op_rev = '/images/icons/hidrant_op_rev.png';
+export const hidrant_op_nrev = '/images/icons/hidrant_op_nrev.png';
+export const hidrant_nop_rev = '/images/icons/hidrant_nop_rev.png';
+export const hidrant_nop_nrev = '/images/icons/hidrant_nop_nrev.png';
+export const hidrant_no_info = '/images/icons/hidrant_no_info.png';
+export const hidrant_shadow = '/images/icons/marker-shadow.png';
+
 function getHydrantIcon(properties: Record<string, any>): L.Icon {
   const currentYear = new Date().getFullYear();
   const surveyDate = properties['survey:date'];
@@ -12,18 +19,17 @@ function getHydrantIcon(properties: Record<string, any>): L.Icon {
   // Ruta a les icones (Leaflet en porta una per defecte)
   let iconUrl = '';
 
-  if (isActive && isCurrentYear) iconUrl = '/images/icons/marker-icon-blue.png';
-  else if (isActive) iconUrl = '/images/icons/marker-icon-grey.png';
-  else if (isDisused && isCurrentYear)
-    iconUrl = '/images/icons/marker-icon-red.png';
-  else iconUrl = '/images/icons/marker-icon-orange.png';
+  if (isActive && isCurrentYear) iconUrl = hidrant_op_rev;
+  else if (isActive) iconUrl = hidrant_op_nrev;
+  else if (isDisused && isCurrentYear) iconUrl = hidrant_nop_rev;
+  else iconUrl = hidrant_nop_nrev;
 
   return new L.Icon({
     iconUrl,
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
-    shadowUrl: '/images/icons/marker-shadow.png',
+    shadowUrl: hidrant_shadow,
     shadowSize: [41, 41],
   });
 }

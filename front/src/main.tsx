d92d606e241bed61a19b99f4185e64105002f9d9
Toni Kny <84742282+tonikny@users.noreply.github.com>
2026-08-09 21:41:46 +0200

    Diferents mides de text (#34)

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -4,6 +4,9 @@ import App from './App';
 import './styles/index.css';
 import 'leaflet/dist/leaflet.css';
 import 'react-toastify/dist/ReactToastify.css';
+import { getFontScale, setFontScale } from './utils/fontScale';
+
+setFontScale(getFontScale());
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
